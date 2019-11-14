c304cec2b2c37e68e17b8e6c8aa5205461464535
Jefferson Jones <jeff@beartooth.com>
2019-11-14 13:42:35 -0800

    remove unnecessary comment

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="agent-finder-masterjefferson",  # Replace with your own username
+    name="agent-finder-masterjefferson",
     version="0.0.1",
     author="Jefferson Jones",
     author_email="jeffersonmjones92@gmail.com",
