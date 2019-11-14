9d88d1df075c3dabe1e50d34ad0d4f6cde47b78b
Jefferson Jones <jeff@beartooth.com>
2019-11-14 13:44:24 -0800

    set correct version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="agent-finder-masterjefferson",
-    version="0.0.1",
+    version="0.1",
     author="Jefferson Jones",
     author_email="jeffersonmjones92@gmail.com",
     description="Find nearby auto insurance agents and dump them to a CSV file.",
